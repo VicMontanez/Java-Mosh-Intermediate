c7ffce52dbba96df1109d2ae8bd40cbdf1b3578c
VicMontanez <v_montanez@hotmail.com>
2020-03-24 00:32:24 -0400

    Procedural Programming

--- Classes/src/com/intro/Main.java
+++ Classes/src/com/intro/Main.java
@@ -3,12 +3,16 @@ package com.intro;
 public class Main {
 
     public static void main(String[] args) {
-        var textBox1 = new TextBox();
-        textBox1.setText("Box 1");
-        System.out.println(textBox1.text.toUpperCase());
+        int baseSalary = 50_000;
+        int extraHours = 10;
+        int hourlyRate = 20;
+    }
 
-        var textBox2 = new TextBox();
-        textBox2.setText("Box 2");
-        System.out.println(textBox2.text);
+    public static int calculateWage(
+            int baseSalary,
+            int extraHours,
+            int hourlyRate
+    ) {
+        return baseSalary + (extraHours * hourlyRate);
     }
 }
