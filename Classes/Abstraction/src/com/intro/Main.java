9427ddf2ff4f944c19343e88fdafbc26928446f1
VicMontanez <v_montanez@hotmail.com>
2020-03-25 00:24:56 -0400

    applied abstraction principals

--- Classes/Abstraction/src/com/intro/Main.java
+++ Classes/Abstraction/src/com/intro/Main.java
@@ -3,7 +3,12 @@ package com.intro;
 public class Main {
 
     public static void main(String[] args) {
-        var browser = new Browser();
-        browser.navigate("127.0.0");
+        //Abstraction example
+//        var browser = new Browser();
+//        browser.navigate("127.0.0");
+        var employee = new Employee(50_000, 20);
+        int wage = employee.calculateWage(10);
+        System.out.println(wage);
+
     }
 }
