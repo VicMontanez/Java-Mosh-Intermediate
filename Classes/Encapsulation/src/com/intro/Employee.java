c0612b69e1808755f7a12c9542d38acb6cecb81b
VicMontanez <v_montanez@hotmail.com>
2020-03-24 20:06:01 -0400

    using getters and setters

--- Classes/Encapsulation/src/com/intro/Employee.java
+++ Classes/Encapsulation/src/com/intro/Employee.java
@@ -8,4 +8,14 @@ public class Employee {
     public int calculateWage(int extraHours) {
         return baseSalary + (hourlyRate * extraHours);
     }
+
+    public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0)
+            throw new IllegalArgumentException("Salary cannot be 0 or less");
+        this.baseSalary = baseSalary;
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
 }
