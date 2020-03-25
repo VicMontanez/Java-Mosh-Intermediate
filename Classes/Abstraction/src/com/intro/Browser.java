b9c50288ffa6b8e1851ef44031a75d8ac520ee91
VicMontanez <v_montanez@hotmail.com>
2020-03-24 22:52:32 -0400

    created method and variable for http request

--- Classes/Abstraction/src/com/intro/Browser.java
+++ Classes/Abstraction/src/com/intro/Browser.java
@@ -3,6 +3,11 @@ package com.intro;
 public class Browser {
     public void navigate(String address) {
         String ip = findIpAddress(address);
+        String html = sendHttpRequest(ip);
+    }
+
+    private String sendHttpRequest(String ip) {
+        return "<html></html>";
     }
 
     public String findIpAddress(String address) {
