1e1becbf621cb000ee0c56f21c175ffc6baba5de
VicMontanez <v_montanez@hotmail.com>
2020-03-25 00:39:44 -0400

    static members

--- Classes/Abstraction/src/com/intro/Main.java
+++ Classes/Abstraction/src/com/intro/Main.java
@@ -6,8 +6,8 @@ public class Main {
         //Abstraction example
 //        var browser = new Browser();
 //        browser.navigate("127.0.0");
-        new Employee(10_000);
         var employee = new Employee(50_000, 20);
+        System.out.println(Employee.numberOfEmployees);
         int wage = employee.calculateWage();
         System.out.println(wage);
 
