591b0459216da4df33a9caf3aa1dbfe47cbff911
VicMontanez <v_montanez@hotmail.com>
2020-03-24 22:40:35 -0400

    created variable that stores ip adress as string

--- Classes/Abstraction/src/com/intro/Browser.java
+++ Classes/Abstraction/src/com/intro/Browser.java
@@ -2,6 +2,10 @@ package com.intro;
 
 public class Browser {
     public void navigate(String address) {
+        String ip = findIpAddress(address);
+    }
 
+    public String findIpAddress(String address) {
+        return "127.0.0.1";
     }
 }
