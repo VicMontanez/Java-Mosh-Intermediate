5f21f108154d4aefad36c09614387990930f3e73
VicMontanez <v_montanez@hotmail.com>
2020-03-25 00:34:35 -0400

    constructor overloading

--- Classes/Abstraction/src/com/intro/Main.java
+++ Classes/Abstraction/src/com/intro/Main.java
@@ -6,6 +6,7 @@ public class Main {
         //Abstraction example
 //        var browser = new Browser();
 //        browser.navigate("127.0.0");
+        new Employee(10_000);
         var employee = new Employee(50_000, 20);
         int wage = employee.calculateWage();
         System.out.println(wage);
