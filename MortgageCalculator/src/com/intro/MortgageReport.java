e5335e75261c4aefc7dfdf752351a8f7dd0fa734
VicMontanez <v_montanez@hotmail.com>
2020-03-26 01:00:50 -0400

    Refactoring - created constructor

--- MortgageCalculator/src/com/intro/MortgageReport.java
+++ MortgageCalculator/src/com/intro/MortgageReport.java
@@ -3,24 +3,31 @@ package com.intro;
 import java.text.NumberFormat;
 
 public class MortgageReport {
-    public static void printMortgage(int principal, float rate, int years){
-        double mortgage = Main.calculateMortgage(principal, rate, years);
+
+    private  MortgageCalculator calculator;
+
+    public MortgageReport(MortgageCalculator calculator) {
+        this.calculator = calculator;
+    }
+
+    public void printPaymentSchedule(){
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("-----------");
+
+    for ( short month = 1; month <= calculator.getYears() * 12; month++) {
+        double payments = calculator.paymentSchedule(month);
+        System.out.println(NumberFormat.getCurrencyInstance().format((payments)));
+
+    }
+}
+
+    public void printMortgage() {
+        double mortgage = calculator.calculateMortgage();
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format((mortgage));
         System.out.println();
         System.out.println("MORTGAGE");
         System.out.println("--------");
         System.out.println("Mortgage is: " + mortgageFormatted);
     }
-
-    public static void printPaymentSchedule(int principal, float rate, int years){
-        System.out.println();
-        System.out.println("PAYMENT SCHEDULE");
-        System.out.println("-----------");
-
-    for ( short month = 1; month <= years * 12; month++) {
-        double payments = Main.paymentSchedule(principal, rate, years, month);
-        System.out.println(NumberFormat.getCurrencyInstance().format((payments)));
-
-    }
-}
 }
