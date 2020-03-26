343949e2d64c25c27f11a2e26ab4cb7f739afa9a
VicMontanez <v_montanez@hotmail.com>
2020-03-26 01:02:48 -0400

    Refactoring - created report variable

--- MortgageCalculator/src/com/intro/Main.java
+++ MortgageCalculator/src/com/intro/Main.java
@@ -13,32 +13,11 @@ public class Main {
         float rate = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
         int years = (int) Console.readNumber("Period (Years): ", 1, 30);
 
-        MortgageReport.printMortgage(principal, rate, years);
-
-        MortgageReport.printPaymentSchedule(principal, rate, years);
+        var calculator = new MortgageCalculator(principal, rate, years);
+        var report = new MortgageReport(calculator);
+        report.printMortgage();
+        report.printPaymentSchedule();
     }
 
 
-    public static double calculateMortgage(int principal,
-                                           float rate,
-                                           int years) {
-        float monthlyRate = rate / 100 / 12;
-        short paymentNumber = (short)(12 * years);
-
-        double mortgage = principal * (monthlyRate * Math.pow(1 + monthlyRate, paymentNumber) / (Math.pow(1 + monthlyRate, paymentNumber) - 1));
-
-        return mortgage;
-    }
-
-    public static double paymentSchedule(int principal, float rate, int years, short numberOfPaymentsMade) {
-        float monthlyRate = rate / 100 / 12;
-        short paymentNumber = (short)(12 * years);
-
-
-
-        double payments = principal * (Math.pow(1 + monthlyRate, paymentNumber) - Math.pow(1 + monthlyRate, numberOfPaymentsMade))/ (Math.pow(1 + monthlyRate, numberOfPaymentsMade) - 1);
-
-        return payments;
-
-    }
 }
