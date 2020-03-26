5a65dec2e3ca1b03227c520eb8f79c49ee48afad
VicMontanez <v_montanez@hotmail.com>
2020-03-26 01:34:19 -0400

    Refactoring - changed int to byte

--- MortgageCalculator/src/com/intro/Main.java
+++ MortgageCalculator/src/com/intro/Main.java
@@ -2,16 +2,11 @@ package com.intro;
 
 
 public class Main {
-    //Principal values between 1K - 1M
-    //Interest rate greater than 0 or less than or equal to 30
-    //Years  value between 1 and 30
-    final static byte MONTHS_IN_YEAR = 12;
-    final static byte PERCENT = 100;
 
     public static void main(String[] args) {
         int principal = (int) Console.readNumber("Principal: ", 1000, 1_000_000);
         float rate = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
-        int years = (int) Console.readNumber("Period (Years): ", 1, 30);
+        byte years = (byte) Console.readNumber("Period (Years): ", 1, 30);
 
         var calculator = new MortgageCalculator(principal, rate, years);
         var report = new MortgageReport(calculator);
