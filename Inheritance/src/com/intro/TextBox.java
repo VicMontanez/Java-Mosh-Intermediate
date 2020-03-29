bbfc96c64ce4a2224118b3790514f75e13d35075
VicMontanez <v_montanez@hotmail.com>
2020-03-29 19:51:50 -0400

    Inheritance - created constructor

--- Inheritance/src/com/intro/TextBox.java
+++ Inheritance/src/com/intro/TextBox.java
@@ -3,6 +3,10 @@ package com.intro;
 public class Textbox extends UIControl{
     private String text = "";
 
+    public Textbox() {
+        System.out.println("Textbox");
+    }
+
     public void setText(String text) {this.text = text}
 
     public void clear() { text = ""; }
