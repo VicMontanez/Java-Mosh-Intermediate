744f60170837a0245fd64018507d7758da84fe90
VicMontanez <v_montanez@hotmail.com>
2020-03-28 23:51:23 -0400

    Inheritance - setbox2 variable to reference same object as box1

--- Inheritance/src/com/intro/Main.java
+++ Inheritance/src/com/intro/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         var box1 = new Textbox();
+        var box2 = box1;
         System.out.println(box1.hashCode());
+        System.out.println(box2.hashCode());
     }
 }
