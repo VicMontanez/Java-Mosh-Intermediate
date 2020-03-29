0f57ed46bf54aad26ee9a2d6014b097fcca02c55
VicMontanez <v_montanez@hotmail.com>
2020-03-28 20:23:19 -0400

    Inheritance - created two methods to enable and disable object

--- Inheritance/src/com/intro/UIControl.java
+++ Inheritance/src/com/intro/UIControl.java
@@ -1,4 +1,13 @@
 package com.intro;
 
 public class UIControl {
+    private boolean isEnabled = true;
+
+    public void enable() {
+        isEnabled = true;
+    }
+
+    public void disable() {
+        isEnabled = false;
+    }
 }
