0b5dba29f3240b52021346401710b8ed97eb30f5
VicMontanez <v_montanez@hotmail.com>
2020-03-29 20:04:23 -0400

    Inheritance - overriding methods

--- Inheritance/src/com/intro/Main.java
+++ Inheritance/src/com/intro/Main.java
@@ -3,9 +3,9 @@ package com.intro;
 public class Main {
 
     public static void main(String[] args) {
-        var box1 = new Textbox();
-        var box2 = box1;
-        System.out.println(box1.hashCode());
-        System.out.println(box2.hashCode());
+        var textBox = new Textbox();
+        textBox.setText("Hello World");
+        System.out.println(textBox);
+
     }
 }
