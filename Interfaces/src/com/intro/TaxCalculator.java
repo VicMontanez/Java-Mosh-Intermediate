e86d2cef7a57bcf221a1ad80529fb78a4d4801fc
VicMontanez <v_montanez@hotmail.com>
2020-04-01 23:28:34 -0400

    Interfaces - created constructor

--- Interfaces/src/com/intro/TaxCalculator.java
+++ Interfaces/src/com/intro/TaxCalculator.java
@@ -3,4 +3,7 @@ package com.intro;
 public class TaxCalculator {
     private double taxableIncome;
 
+    public TaxCalculator(double taxableIncome) {
+        this.taxableIncome = taxableIncome;
+    }
 }
