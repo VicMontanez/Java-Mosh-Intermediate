15ff7a5bd12283d6029f61d77940305fa25ecbbe
VicMontanez <v_montanez@hotmail.com>
2020-04-04 19:59:46 -0400

    Interfaces - constructor injection

--- Interfaces/src/com/intro/Main.java
+++ Interfaces/src/com/intro/Main.java
@@ -3,6 +3,7 @@ package com.intro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	var calculator = new TaxCalculator2020(100_000);
+	var report = new TaxReport(calculator);
     }
 }
